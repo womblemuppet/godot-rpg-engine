3a7c5dd2f975c9635908f9dcd996c43970cbe815
womblemuppet <jeremy.wood.nz@gmail.com>
2026-01-25 14:49:06 +0000

    Link up fight sprites to character models

--- fight/fighter.gd
+++ fight/fighter.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 var display_name: String
 var hp: int
 var status: Character.Status
@@ -9,8 +10,12 @@ var fight_sprites: Dictionary
 
 
 func init(fighter_data):
+  var sprite_2d = $Sprite2D
+  
   display_name = fighter_data.display_name
   hp = fighter_data.hp
   status = fighter_data.status
   movelist = fighter_data.movelist
   fight_sprites = fighter_data.fight_sprites
+  if fight_sprites.has("idle"):
+    sprite_2d.texture = fight_sprites["idle"]
